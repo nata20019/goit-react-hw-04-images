06c572f090fb47de99e0944aae2934a0c616bed0
nata20019 <nbernikova68@gmail.com>
2025-06-12 09:29:05 +0200

    mod43456789

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -47,3 +47,53 @@ export class Modal extends Component {
 }
 
 export default Modal;
+
+// import React, { Component } from 'react';
+// import { createPortal } from 'react-dom';
+// import PropTypes from 'prop-types';
+// import './Modal.css';
+
+// const modalRoot = document.querySelector('#root'); // Це ID елемента в index.html
+
+// export class Modal extends Component {
+//   static propTypes = {
+//     onClose: PropTypes.func.isRequired,
+//     imageURL: PropTypes.string.isRequired,
+//     imageTags: PropTypes.string.isRequired,
+//   };
+
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleOverlayClick = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     const { imageURL, imageTags } = this.props;
+
+//     return createPortal(
+//       <div className="Overlay" onClick={this.handleOverlayClick}>
+//         <div className="Modal">
+//           <img src={imageURL} alt={imageTags} />
+//         </div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
+
+// export default Modal;
